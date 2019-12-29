d18e23fff5ce98455975b4b2d4c16b4fe3a987d3
Rahul Soni <soni_rahul@live.com>
2019-12-29 12:06:42 +0530

    initial commit

---
+++ src/test/java/com/altimetrik/greetingservice/GreetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.greetingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
