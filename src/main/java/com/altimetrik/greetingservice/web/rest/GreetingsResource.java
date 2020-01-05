c43eefe64044da608e2f08f13e8ae7db17045151
Rahul Soni <soni_rahul@live.com>
2020-01-06 04:17:52 +0530

    adding logging and spring cloud sleuth

--- src/main/java/com/altimetrik/greetingservice/web/rest/GreetingsResource.java
+++ src/main/java/com/altimetrik/greetingservice/web/rest/GreetingsResource.java
@@ -1,16 +1,23 @@
 package com.altimetrik.greetingservice.web.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.invoke.MethodHandles;
+
 @RestController
 @RequestMapping("/api")
 public class GreetingsResource {
 
+    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     @GetMapping("/greet")
     public ResponseEntity<String> greet() {
+        log.debug("calling greet method in greeting service");
         return ResponseEntity.ok("Greeting from the greeting service");
     }
 }
