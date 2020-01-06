9419c840ce77b3fefb68f268a5812a65aabca568
Rahul Soni <soni_rahul@live.com>
2020-01-06 23:50:21 +0530

    delay the application start in case of docker container and add eureka to the bootstrap file.

--- src/main/jib/entrypoint.sh
+++ src/main/jib/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+echo "The application will start in ${ALTIMETRIK_SLEEP}s..." && sleep ${ALTIMETRIK_SLEEP}
 exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.altimetrik.greetingservice.GreetingServiceApplication"  "$@"
