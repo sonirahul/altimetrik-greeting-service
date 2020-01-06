466d2de31f4a0167ea273a0907befb74c1782af3
Rahul Soni <soni_rahul@live.com>
2020-01-06 17:09:05 +0530

    Swagger configuration

---
+++ src/main/java/com/altimetrik/greetingservice/config/SwaggerConfiguration.java
@@ -0,0 +1,85 @@
+package com.altimetrik.greetingservice.config;
+
+import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Bean
+    public Docket swaggerSpringfoxDocket() {
+        log.debug("Starting Swagger");
+        Contact contact = new Contact(
+                "Rahul Soni",
+                "http://example.com",
+                "soni_rahul@live.com");
+
+        List<VendorExtension> vext = new ArrayList<>();
+        ApiInfo apiInfo = new ApiInfo(
+                "Altimetrik Backend API",
+                "Altimetrik Backend API description",
+                "1.0",
+                "http://example.com",
+                contact,
+                "MIT",
+                "http://example.com",
+                vext);
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.regex(Constants.DEFAULT_INCLUDE_PATTERN))
+                .build()
+                .apiInfo(apiInfo)
+                .pathMapping("/")
+                .forCodeGeneration(true)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .ignoredParameterTypes(SpringDataWebProperties.Pageable.class)
+                .ignoredParameterTypes(java.sql.Date.class)
+                .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
+                .directModelSubstitute(java.time.LocalDateTime.class, Date.class)
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .useDefaultResponseMessages(false);
+    }
+
+
+    private ApiKey apiKey() {
+        return new ApiKey(Constants.JWT, Constants.AUTHORIZATION, Constants.HEADER);
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(Constants.DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference(Constants.JWT, authorizationScopes));
+    }
+}
